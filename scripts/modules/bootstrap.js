258f990588a6818ba1b5017f44037cf08830340e
Tom Kershaw <tom@twotwentytwo.co.uk>
2014-04-18 12:58:53 +0100

    adding sass and grunt, new images et al

---
+++ scripts/modules/bootstrap.js
@@ -0,0 +1,5 @@
+define(["../vendor/jquery-1.10.2.min", "responsive-carousel", "responsive-carousel.pagination","responsive-carousel.autoinit"], function($, responsivecarousel, rc_pagination, rc_init) {
+	responsivecarousel;
+	rc_pagination;
+	rc_init;
+});
